feeb06749dcc18c1f72188e5612931787f60b61a
Jason Grout <jason.grout@drake.edu>
2014-01-23 23:47:03 -0600

    Update for recent changes in IPython

--- pythreejs.py
+++ pythreejs.py
@@ -4,118 +4,103 @@ from IPython.utils.traitlets import (Unicode, Int, Instance, Enum, List, Float,
                                      Any, CFloat, Bool, This, CInt)
 import numpy
 
-def vector3(trait_type, default=None):
+def vector3(trait_type, default=None, **kwargs):
     if default is None: 
         default=[0,0,0]
     return List(trait_type, default_value=default, 
-                minlen=3, maxlen=3, allow_none=False)
+                minlen=3, maxlen=3, allow_none=False, **kwargs)
 
 
 class Object3d(Widget):
     """
     If matrix is not None, it overrides the position, rotation, scale, and up variables.
     """
-    keys = ['position', 'rotation', 'scale', 'up', 
-             'visible', 'castShadow', 'receiveShadow', 
-             'matrix',
-             'children'] + Widget.keys
-    position = vector3(CFloat)
-    rotation = vector3(CFloat)
-    scale = vector3(CFloat, [1,1,1])
-    up = vector3(CFloat, [0,1,0])
-    visible = Bool(True)
-    castShadow = Bool(False)
-    receiveShadow = Bool(False)
-    matrix = List(CFloat)
+    position = vector3(CFloat, sync=True)
+    rotation = vector3(CFloat, sync=True)
+    scale = vector3(CFloat, [1,1,1], sync=True)
+    up = vector3(CFloat, [0,1,0], sync=True)
+    visible = Bool(True, sync=True)
+    castShadow = Bool(False, sync=True)
+    receiveShadow = Bool(False, sync=True)
+    matrix = List(CFloat, sync=True)
     # TODO: figure out how to get a list of instances of Object3d
-    children = List(trait=None, default_value=[], allow_none=False)
+    children = List(trait=None, default_value=[], allow_none=False, sync=True)
 
 class Controls(Widget):
-    view_name = Unicode('ControlsView')
-    keys = ['controlling'] + Widget.keys
-    controlling = Instance(Object3d)
+    _view_name = 'ControlsView'
+    controlling = Instance(Object3d, sync=True)
 
 class OrbitControls(Controls):
-    view_name = Unicode('OrbitControlsView')
+    _view_name = 'OrbitControlsView'
     
 class Geometry(Widget):
-    view_name = Unicode('GeometryView')
+    _view_name = 'GeometryView'
 
 class SphereGeometry(Geometry):
-    view_name = Unicode('SphereGeometryView')
-    keys = ['radius'] + Geometry.keys
-    radius = CFloat(1)
+    _view_name = 'SphereGeometryView'
+    radius = CFloat(1, sync=True)
 
 class SurfaceGeometry(Geometry):
     """
     A regular grid with heights
     """
-    view_name = Unicode('SurfaceGeometryView')
-    keys = ['z', 'width', 'height', 'width_segments', 'height_segments'] + Geometry.keys
-    z = List(CFloat, [0]*100)
-    width = CInt(10)
-    height = CInt(10)
-    width_segments = CInt(10)
-    height_segments = CInt(10)
+    _view_name = 'SurfaceGeometryView'
+    z = List(CFloat, [0]*100, sync=True)
+    width = CInt(10, sync=True)
+    height = CInt(10, sync=True)
+    width_segments = CInt(10, sync=True)
+    height_segments = CInt(10, sync=True)
 
 class Material(Widget):
-    view_name = Unicode('MaterialView')
-    keys = ['color', 'opacity', 'wireframe'] + Widget.keys
-    color = Any('yellow')
-    opacity = CFloat(1.0)
-    wireframe = Bool(False)
+    _view_name = 'MaterialView'
+    color = Any('yellow', sync=True)
+    opacity = CFloat(1.0, sync=True)
+    wireframe = Bool(False, sync=True)
 
 class Mesh(Object3d):
-    view_name = Unicode('MeshView')
-    keys = ['geometry', 'material'] + Object3d.keys
-    geometry = Instance(Geometry)
-    material = Instance(Material)
+    __view_name = 'MeshView'
+    geometry = Instance(Geometry, sync=True)
+    material = Instance(Material, sync=True)
 
 class Camera(Object3d):
-    view_name = Unicode('CameraView')
-    keys = ['fov', 'ratio'] + Object3d.keys
-    fov = CFloat(40)
-    ratio = CFloat(600.0/400.0)
+    _view_name = 'CameraView'
+    fov = CFloat(40, sync=True)
+    ratio = CFloat(600.0/400.0, sync=True)
     
 class Scene(Object3d):
-    view_name = Unicode('SceneView') 
+    _view_name = 'SceneView'
     
 class Renderer(DOMWidget):
-    view_name = Unicode('RendererView')
-    keys = ['width', 'height', 'renderer_type', 'scene', 'camera', 'controls'] + DOMWidget.keys
-    width = CInt(600)
-    height = CInt(400)
-    renderer_type = Enum(['webgl', 'canvas', 'auto'], 'auto')
-    scene = Instance(Scene)
-    camera = Instance(Camera)
-    controls = Instance(Controls)
+    _view_name = 'RendererView'
+    width = CInt(600, sync=True)
+    height = CInt(400, sync=True)
+    renderer_type = Enum(['webgl', 'canvas', 'auto'], 'auto', sync=True)
+    scene = Instance(Scene, sync=True)
+    camera = Instance(Camera, sync=True)
+    controls = Instance(Controls, sync=True)
 
     
 class Light(Object3d):
-    keys = ['color']+Object3d.keys
-    color = Any('white') # could be string or number or tuple
+    color = Any('white', sync=True) # could be string or number or tuple
 
 class AmbientLight(Light):
-    view_name = 'AmbientLight'
+    _view_name = 'AmbientLight'
 
 class PositionLight(Light):
-    keys = ['intensity'] + Light.keys
-    view_name = 'PositionLight'
-    intensity = CFloat(1)
+    _view_name = 'PositionLight'
+    intensity = CFloat(1, sync=True)
     
 class DirectionalLight(PositionLight):
-    view_name = 'DirectionalLight'
+    _view_name = 'DirectionalLight'
 
 class PointLight(PositionLight):
-    keys = ['distance'] + PositionLight.keys
-    view_name = 'PointLight'
-    distance = CFloat()
+    _view_name = 'PointLight'
+    distance = CFloat(10, sync=True)
 
 class SpotLight(PointLight):
-    keys = ['angle', 'exponent'] + PointLight.keys
-    view_name = 'SpotLight'
-    angle = CFloat()
-    exponent = CFloat()
+    _view_name = 'SpotLight'
+    angle = CFloat(10, sync=True)
+    exponent = CFloat(0.5, sync=True)
     
 
 lights = {
