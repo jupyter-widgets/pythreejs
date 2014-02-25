cba08a83bb32acfd845c0593c3597d9e6053c670
DavidMascharka <davidmascharka@gmail.com>
2014-02-25 00:26:17 -0600

    Add attributes to line basic material

--- pythreejs.js
+++ pythreejs.js
@@ -451,7 +451,7 @@ require(["threejs-all", "notebook/js/widgets/widget"], function() {
         new_properties: function() {
             MaterialView.prototype.new_properties.call(this);
             this.enum_properties.push('vertexColors');
-            this.set_properties.push('color');
+            this.set_properties.push('color', 'linecap', 'linejoin');
             this.scalar_properties.push('linewidth', 'fog');
         },
         new_obj: function() {return new THREE.LineBasicMaterial();},
