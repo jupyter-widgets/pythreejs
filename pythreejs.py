a85348a6c70de6f48beeb0b429de48840ef626e9
DavidMascharka <davidmascharka@gmail.com>
2014-02-28 00:27:34 -0600

    Fix shader material

--- pythreejs.py
+++ pythreejs.py
@@ -282,8 +282,8 @@ class ParticleSystemMaterial(Material):
 
 class ShaderMaterial(Material):
     _view_name = Unicode('ShaderMaterialView', sync=True)
-    fragmentShader = Unicode('int main(){ }', sync=True)
-    vertexShader = Unicode('int main(){ }', sync=True)
+    fragmentShader = Unicode('void main(){ }', sync=True)
+    vertexShader = Unicode('void main(){ }', sync=True)
     morphTargets = Bool(False, sync=True)
     lights = Bool(False, sync=True)
     morphNormals = Bool(False, sync=True)
