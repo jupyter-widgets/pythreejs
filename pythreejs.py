b335d45b5d1c5f9ff4485cb771290ed240784d2a
Joel Venzke <Joel.Venzke@Drake.edu>
2014-03-14 09:39:12 -0500

    sage graphics

--- pythreejs.py
+++ pythreejs.py
@@ -456,9 +456,9 @@ class SpotLight(PointLight):
 class SageGraphics(Mesh):
     plot = Instance('sage.plot.plot3d.base.Graphics3d')
     # TODO material type option
-    dispatch{values:fn,
-        'object': 
-    }
+    # dispatch{values:fn,
+    #     'object': 
+    # }
 
     def _plot_changed(self, name, old, new):
         self.type = new.scenetree_json()['type']
