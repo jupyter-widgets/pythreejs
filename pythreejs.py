cf12699363f4a809a2b4ab1cbfe733587daea7ac
Joel Venzke <Joel.Venzke@Drake.edu>
2014-03-14 19:10:18 -0500

    added plot back

--- pythreejs.py
+++ pythreejs.py
@@ -492,7 +492,7 @@ def create_from_plot(plot):
     # elif(plotType == 'box'):
     #     geometry = geometry_from_box(plot)
     
-    material = dispatch[plotType]()
+    material = dispatch[plotType](plot)
     #material = material(plot)
     geometry = geometry_from_box(plot)
 
