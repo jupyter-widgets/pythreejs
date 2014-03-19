550be637717a0d37017b438255e22f09fe8f6e7b
Joel Venzke <Joel.Venzke@Drake.edu>
2014-03-19 12:37:37 -0500

    flaten vertices

--- pythreejs.py
+++ pythreejs.py
@@ -510,7 +510,7 @@ def json_index_face_set(t):
     from itertools import chain
     def flatten(ll):
         return list(chain.from_iterable(ll))
-    return FaceGeometry(vertices = t['vertices'],
+    return FaceGeometry(vertices = flatten(t['vertices']),
                          face3 = flatten(t['face3']),
                          face4 = flatten(t['face4']),
                          facen = flatten(t['facen']))
