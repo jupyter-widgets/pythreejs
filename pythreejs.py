9ef3417bed9f050c1d80b0b44174e6eaff37b282
Jason Grout <jason.grout@drake.edu>
2014-03-26 00:37:17 -0500

    Take the transpose of transformation matrices from sage.
    It still doesn't seem to be transforming arrow3d((0,0,0),(0,0,1)) correctly, though.

--- pythreejs.py
+++ pythreejs.py
@@ -533,8 +533,10 @@ def json_object(t):
     return mesh
 
 def json_group(t):
-    m = t['matrix']
-    # TODO transpose m
+    m = t.get('matrix', [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1])
+    # take the transpose of m -- is this working?
+    m[1], m[2], m[3], m[4], m[6], m[7], m[8], m[9],m[11],m[12],m[13],m[14] = \
+    m[4], m[8],m[12], m[1], m[9],m[13], m[2], m[6],m[14], m[3], m[7],m[11]
     children = [sage_handlers[c['type']](c) for c in t['children']]
     return Object3d(matrix=m, children=children)
 
