ea22ae760f2a3ffd70050e685553c88be6fe9b06
Joel Venzke <Joel.Venzke@Drake.edu>
2014-03-28 16:52:56 -0500

    color to white for testing

--- pythreejs.py
+++ pythreejs.py
@@ -593,7 +593,7 @@ def json_line(t):
 def json_text(t):
     tree_geometry = t['geometry']
     tree_texture = t['texture']
-    tt = TextTexture(string=tree_geometry['string'], color=tree_texture['color'])
+    tt = TextTexture(string=tree_geometry['string'], color='white')
     sm = SpriteMaterial(map=tt, opacity=tree_texture['opacity'], transparent = tree_texture['opacity'] < 1 )
     return Sprite(material=sm, scaleToTexture=True)
 
