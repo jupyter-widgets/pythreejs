abbe87c3849c6f0e45dadfebd05d41d8d33c0b07
Jason Grout <jason.grout@drake.edu>
2014-04-01 13:34:51 -0500

    Get transformation matrices working correctly
    Before, if a transformation matrix was set, it was overridden the next time the object was updated.  Now it disable's three.js' automatic updates if the matrix is explicitly set.

--- pythreejs.py
+++ pythreejs.py
@@ -543,7 +543,7 @@ def json_object(t):
 
 def json_group(t):
     m = t.get('matrix', [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1])
-    # take the transpose of m -- is this working?
+    # three.js transformation matrices are the transpose of sage transformations
     m[1], m[2], m[3], m[4], m[6], m[7], m[8], m[9],m[11],m[12],m[13],m[14] = \
     m[4], m[8],m[12], m[1], m[9],m[13], m[2], m[6],m[14], m[3], m[7],m[11]
     children = [sage_handlers[c['type']](c) for c in t['children']]
