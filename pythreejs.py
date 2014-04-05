49f322b80a65baee894ce03125ac43e86e3c34a5
Joel Venzke <Joel.Venzke@Drake.edu>
2014-04-04 22:17:14 -0500

    working on backwards compatablity

--- pythreejs.py
+++ pythreejs.py
@@ -634,7 +634,7 @@ def json_line(t):
     #                             position=midpoint,
     #                             scale=[.02,1,.02],
     #                             rotation=rotate))
-    return Mesh(geometry=g, material=m, scale=[.2,.2,.2])
+    return Mesh(geometry=g, material=m, scale=[1,1,1])
 
 def json_text(t):
     tree_geometry = t['geometry']
