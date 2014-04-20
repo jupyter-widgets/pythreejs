4ed0283699ef8b7a2ec943e9f2665a13f5ca6e7f
Joel Venzke <Joel.Venzke@Drake.edu>
2014-04-19 20:12:11 -0500

    fix point scaling

--- pythreejs.py
+++ pythreejs.py
@@ -666,7 +666,7 @@ def json_text(t):
 def json_point(t):
     g = SphereGeometry(radius=t['geometry']['size'])
     m = sage_handlers['texture'](t['texture'])
-    myobject = Mesh(geometry=g, material=m, scale=[.02,.02,.02])
+    myobject = Mesh(geometry=g, material=m, scale=[.002,.002,.002])
     return ScaledObject(children=[myobject], position = list(t['geometry']['position']))
 
 sage_handlers = {'object' : json_object,
