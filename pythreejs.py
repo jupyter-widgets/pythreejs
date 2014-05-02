9df24a29baef9f71e6957f0d23fe6c4146c66648
Jason Grout <jason.grout@drake.edu>
2014-05-02 10:13:39 -0500

    Fix sage graphics controls

--- pythreejs.py
+++ pythreejs.py
@@ -593,7 +593,7 @@ def create_from_plot(plot):
     cam = PerspectiveCamera(position=[10,10,10], fov=40, up=[0,0,1],
            children=[DirectionalLight(color=0xffffff, position=[3,5,1], intensity=0.5)])
     scene = Scene(children=[obj, AmbientLight(color=0x777777)])
-    renderer = Renderer(camera=cam, scene=scene, controls=OrbitControls(controlling=cam), color='white')
+    renderer = Renderer(camera=cam, scene=scene, controls=[OrbitControls(controlling=cam)], color='white')
     return renderer
 
 def json_object(t):
