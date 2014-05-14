d086b4f6a79990b1eca3e5a07083a6dec3b124c1
Jason Grout <jason.grout@drake.edu>
2014-05-14 17:31:07 -0500

    Move arrow back so that it ends at the ending point of the line.
    Also, shortened the code a bit by using the path we already calculate.
    
    Example:
    
    html("<script src='http://boxen.math.washington.edu/home/jason/pythreejs/pythreejs.js'></script>")
    load('http://boxen.math.washington.edu/home/jason/pythreejs/pythreejs.py')
    from time import sleep
    sleep(1)
    
    from sage.plot.plot3d.shapes2 import Line
    
    path = [(sin(i), cos(i), i/10) for i in srange(0,10,.1)]
    p = Line(path, arrow_head=True)
    p+= point(path[-1], color='red')
    show(create_from_plot(p))

--- pythreejs.py
+++ pythreejs.py
@@ -696,23 +696,22 @@ def json_sphere(t):
 def json_line(t):
     tree_geometry = t['geometry']
     m = sage_handlers['texture'](t['texture'])
-    path = []
     mesh = []
-    for p in tree_geometry['points']:
-        path.append(list(p))
+    path = [list(p) for p in tree_geometry['points']]
     mesh.append(Mesh(material=m, geometry=TubeGeometry(path=path, radialSegments=50, radius=.01*tree_geometry['thickness'])))
     c = Mesh(material=m, geometry=CircleGeometry(segments=50, radius=.01*tree_geometry['thickness']))
-    c.look_at(list(tree_geometry['points'][0]), list(tree_geometry['points'][1]))
-    c.position = list(tree_geometry['points'][0])
+    c.look_at(path[0], path[1])
+    c.position = path[0]
     mesh.append(c)
     if (tree_geometry['arrowhead']):
+        height = .03*tree_geometry['thickness']
         c = Mesh(material=m, 
                     geometry=CylinderGeometry(radiusTop=0,
-                                                 radiusBottom=.01*tree_geometry['thickness'],
-                                                 height=.02*tree_geometry['thickness'],
+                                                 radiusBottom=.02*tree_geometry['thickness'],
+                                                 height=height,
                                                  up=[1,0,0],
                                                  radiusSegments=50))
-        c.look_at(list(tree_geometry['points'][-1]), list(tree_geometry['points'][-2]))
+        c.look_at(path[-1], path[-2])
         q1 = c.quaternion
         q2 = [0.7071067811865475, 0.0, 0.0, 0.7071067811865476]
         # http://www.mathworks.com/help/aeroblks/quaternionmultiplication.html
@@ -720,16 +719,26 @@ def json_line(t):
                         q2[3]*q1[1]+q2[0]*q1[2]+q2[1]*q1[3]-q2[2]*q1[0],
                         q2[3]*q1[2]-q2[0]*q1[1]+q2[1]*q1[0]+q2[2]*q1[3],
                         q2[3]*q1[3]-q2[0]*q1[0]-q2[1]*q1[1]-q2[2]*q1[2]]
-        p1 = tree_geometry['points'][-1]
-        p2 = tree_geometry['points'][-2]
+        p1 = path[-1]
+        p2 = path[-2]
         d = [p1[0]-p2[0], p1[1]-p2[1], p1[2]-p2[2]]
-        length = sqrt(d[0]*d[0]+d[1]*d[1]+d[2]*d[2])/(.01*tree_geometry['thickness'])
-        d = [d[0]/length, d[1]/length, d[2]/length]
-        c.position = [p1[0]+d[0], p1[1]+d[1], p1[2]+d[2]]
+        last_seg = sqrt(d[0]*d[0]+d[1]*d[1]+d[2]*d[2])
+        adjust_cone = last_seg/(height/2)
+        d = [i/adjust_cone for i in d]
+        c.position = [p1[0]-d[0], p1[1]-d[1], p1[2]-d[2]]
+        d2 = [i*2 for i in d]
+        if last_seg>adjust_cone*2:
+            # shorten last segment
+            mesh[0].geometry.path[-1] = [p1[0]-d2[0], p1[1]-d2[1], p1[2]-d2[2]]
+        else:
+            # remove last segment
+            mesh[0].geometry.path.pop()
+            # TODO: keep removing points until we have removed enough that the arrow fits on without
+            # seeing the line sticking through the arrow
     else:
         c = Mesh(material=m, geometry=CircleGeometry(segments=50, radius=.01*tree_geometry['thickness']))
-        c.look_at(list(tree_geometry['points'][-1]), list(tree_geometry['points'][-2]))
-        c.position = list(tree_geometry['points'][-1])
+        c.look_at(path[-1], path[-2])
+        c.position = path[-1]
     mesh.append(c)
     return Object3d(children=mesh)
 
