45c712fc1242df1e5380963236fe9da126b455fe
Jason Grout <jgrout6@bloomberg.net>
2014-11-10 16:56:22 +0000

    Add ipython dependency

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(name='pythreejs',
       version='0.1',
+      install_requires = ['ipython'],
       url='https://github.com/jasongrout/pythreejs',
       packages=['pythreejs'],
       zip_safe=False)
