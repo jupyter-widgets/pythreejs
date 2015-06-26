2b9292c055239d17ea44ff6aee3d8bf39ed3d25b
Jason Grout <jason@jasongrout.org>
2015-06-26 02:59:30 -0400

    add a few more things to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
-from setuptools import setup
+# -*- coding: utf-8 -*-
 
-setup(name='pythreejs',
-      version='0.1',
-      install_requires = ['ipython'],
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name = 'pythreejs',
+      version = '0.1',
+      description='Interactive 3d graphics for the Jupyter notebook, using Three.js from IPython widgets.',
+      author='PyThreejs Development Team',
+      author_email='jason@jasongrout.org',
+      license='BSD',
       url='https://github.com/jasongrout/pythreejs',
+      install_requires = ['ipywidgets'],
       packages=['pythreejs'],
+      include_package_data=True,
+      keywords=['ipython', 'jupyter', 'widgets', 'webgl', 'graphics', '3d'],
       zip_safe=False)
