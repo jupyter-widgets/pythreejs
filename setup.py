186600ca246d0b756c729e371e416fe3d4b21392
Jason Grout <jason@jasongrout.org>
2015-07-07 11:10:17 -0400

    Fix setup.py to work (and assume setuptools is installed)

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 setup(name = 'pythreejs',
-      version = '0.1',
+      version = '0.1.0',
       description='Interactive 3d graphics for the Jupyter notebook, using Three.js from IPython widgets.',
       author='PyThreejs Development Team',
       author_email='jason@jasongrout.org',
       license='BSD',
       url='https://github.com/jasongrout/pythreejs',
-      install_requires = ['ipywidgets'],
+      install_requires = ['ipython'],
       packages=['pythreejs'],
       include_package_data=True,
       keywords=['ipython', 'jupyter', 'widgets', 'webgl', 'graphics', '3d'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Programming Language :: Python'],
       zip_safe=False)
