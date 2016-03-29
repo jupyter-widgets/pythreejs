ab81eb1dcef8bb8eb47a140b07baf53e248d0ff3
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-03-29 14:06:57 -0400

    Fixes in setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,31 @@
 
 from distutils.core import setup
 
-setup(name = 'pythreejs',
-      version = '0.1.19',
-      description='Interactive 3d graphics for the Jupyter notebook, using Three.js from IPython widgets.',
-      long_description='A Python/ThreeJS bridge utilizing the IPython widget infrastructure.',
+setup(name='pythreejs',
+      version='0.2.0',
+      description='Interactive 3d graphics for the Jupyter notebook, using Three.js from Jupyter interactive widgets.',
+      long_description='A Python/ThreeJS bridge utilizing the Jupyter widget infrastructure.',
       author='PyThreejs Development Team',
       author_email='jason@jasongrout.org',
       license='BSD',
-      url='https://github.com/jasongrout/pythreejs',
-      requires = ['ipython'],
+      url='https://github.com/jovyan/pythreejs',
+      requires=[
+          'ipywidgets (>4.1.1)'
+          ],
       packages=['pythreejs'],
-      package_data={'pythreejs': [
-      'nbextension/pythreejs.js', 
-      'nbextension/three.js/*.md',
-      'nbextension/three.js/LICENSE',
-      'nbextension/three.js/build/three.js',
-      'nbextension/three.js/examples/js/*.js',
-      'nbextension/three.js/examples/js/controls/*.js',
-      'nbextension/three.js/examples/js/renderers/*.js'
-      ]},
+      package_data={
+          'pythreejs': [
+              'nbextension/pythreejs.js',
+              'nbextension/three.js/*.md',
+              'nbextension/three.js/LICENSE',
+              'nbextension/three.js/build/three.js',
+              'nbextension/three.js/examples/js/*.js',
+              'nbextension/three.js/examples/js/controls/*.js',
+              'nbextension/three.js/examples/js/renderers/*.js'
+          ]
+      },
       keywords=['ipython', 'jupyter', 'widgets', 'webgl', 'graphics', '3d'],
-      classifiers=['Development Status :: 4 - Beta',
-                   'Programming Language :: Python']
-      )
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python'
+      ])
