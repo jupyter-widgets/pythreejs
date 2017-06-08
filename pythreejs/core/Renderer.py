8fd7ff0b4e4b5a5bd3d8d9b1d5715b4d8a8580f3
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-06-08 14:04:14 +0200

    Renderer dimensions link

--- pythreejs/core/Renderer.py
+++ pythreejs/core/Renderer.py
@@ -1,5 +1,8 @@
-from ipywidgets import Widget, DOMWidget, widget_serialization, Color
-from traitlets import Unicode, Int, CInt, Instance, This, Enum, List, Dict, Float, CFloat, Bool, Tuple
+"""
+"""
+from ipywidgets import widget_serialization, Color
+from traitlets import (
+    Unicode, CInt, Instance, Float, Tuple, link)
 
 from ..enums import *
 from ..traits import *
@@ -21,14 +24,19 @@ class Renderer(RenderableWidget):
     _view_name = Unicode('RendererView').tag(sync=True)
     _model_name = Unicode('RendererModel').tag(sync=True)
 
-    width = CInt(200).tag(sync=True)
-    height = CInt(200).tag(sync=True)
+    width = CInt(200)
+    height = CInt(200)
     scene = Instance(Scene).tag(sync=True, **widget_serialization)
     camera = Instance(Camera).tag(sync=True, **widget_serialization)
     controls = Tuple(Instance(Controls)).tag(sync=True, **widget_serialization)
     #effect = Instance(Effect, allow_none=True).tag(sync=True, **widget_serialization)
     background = Color('black', allow_none=True).tag(sync=True)
     background_opacity = Float(min=0.0, max=1.0).tag(sync=True)
+
+    def __init__(self, **kwargs):
+        super(Renderer, self).__init__(**kwargs)
+        link((self, 'width'), (self, '_width'))
+        link((self, 'height'), (self, '_height'))
 
     def render(self, scene, camera):
         content = {
