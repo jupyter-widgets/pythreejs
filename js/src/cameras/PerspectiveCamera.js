bc96e1c890f1a9ce2f7801cd72797b7479293364
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-06-13 13:52:03 +0200

    Ensure cameras update their projection matrix

---
+++ js/src/cameras/PerspectiveCamera.js
@@ -0,0 +1,15 @@
+var _ = require('underscore');
+var PerspectiveCameraAutogen = require('./PerspectiveCamera.autogen');
+
+var PerspectiveCameraModel = PerspectiveCameraAutogen.PerspectiveCameraModel.extend({
+    // push data from model to three object
+    syncToThreeObj: function() {
+        PerspectiveCameraAutogen.PerspectiveCameraModel.prototype.syncToThreeObj.call(this);
+        // Always update the projection matrix after setting the attributes:
+        this.obj.updateProjectionMatrix();
+    }
+});
+
+module.exports = {
+    PerspectiveCameraModel: PerspectiveCameraModel,
+};
