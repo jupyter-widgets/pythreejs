5877b8f6f486af8b56cf77a1fe4e147206bbbf1b
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-06-15 15:40:30 +0200

    Ensure TextTexture can be updated

--- js/src/textures/TextTexture.js
+++ js/src/textures/TextTexture.js
@@ -1,47 +1,60 @@
 var _ = require('underscore');
 var Promise = require('bluebird');
-var TextTextureBase = require('./TextTexture.autogen');
+var TextTextureBase = require('./TextTexture.autogen').TextTextureModel;
 
-var TextTextureModel = TextTextureBase.TextTextureModel.extend({
-
-    // TODO: Replace sync function
+var TextTextureModel = TextTextureBase.extend({
 
     constructThreeObjectAsync: function() {
 
+        var self = this;
+
+        var p = new Promise(function(resolve, reject) {
+            var canvas = self.buildCanvas();
+            return resolve(new THREE.CanvasTexture(canvas));
+        });
+        return p;
+    },
+
+    // push data from model to three object
+    syncToThreeObj: function() {
+        TextTextureBase.prototype.syncToThreeObj.call(this);
+
+        // TODO: Use mapping of relevant properties instead of sync?
+        var canvas = this.buildCanvas();
+        this.obj.image = canvas;
+        this.obj.needsUpdate = true;
+        this.set({ version: this.obj.version }, 'pushFromThree');
+    },
+
+    buildCanvas: function() {
         var fontFace = this.get('fontFace');
         var size = this.get('size');
         var color = this.get('color');
         var string = this.get('string');
 
-        var self = this;
+        var canvas = document.createElement('canvas');
+        var context = canvas.getContext('2d');
 
-        var p = new Promise(function(resolve, reject) {
-            var canvas = document.createElement('canvas');
-            var context = canvas.getContext('2d');
+        canvas.height = size;
+        var font = 'Normal ' + size + 'px ' + fontFace;
+        context.font = font;
 
-            canvas.height = size;
-            var font = 'Normal ' + size + 'px ' + fontFace;
-            context.font = font;
+        var metrics = context.measureText(string);
+        var textWidth = Math.ceil(metrics.width);
+        canvas.width = textWidth;
 
-            var metrics = context.measureText(string);
-            var textWidth = metrics.width;
-            canvas.width = textWidth;
+        if (this.get('squareTexture')) {
+            canvas.height = canvas.width;
+        }
 
-            if (self.get('squareTexture')) {
-                canvas.height = canvas.width;
-            }
-
-            context.textAlign = 'center';
-            context.textBaseline = 'middle';
-            context.fillStyle = color;
-            // Must set the font again for the fillText call
-            context.font = font;
-            context.fillText(string, canvas.width / 2, canvas.height / 2);
-
-            return resolve(new THREE.CanvasTexture(canvas));
-        });
-        return p;
-    },
+        context.textAlign = 'center';
+        context.textBaseline = 'middle';
+        context.fillStyle = color;
+        // Must set the font again for the fillText call
+        context.font = font;
+        context.fillText(string, canvas.width / 2, canvas.height / 2);
+        return canvas
+    }
 
 });
 
