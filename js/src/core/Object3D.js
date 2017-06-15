f744fbe65eaa63b3ac1d63a36129dadd7456bf90
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-06-15 15:22:42 +0200

    Prevent infinite recursion via Object3D.parent
    If there are other such circular references, we should probably consider
    using some more generic machinery, but if it is only parent this simple
    hard-coded way should be sufficient.

--- js/src/core/Object3D.js
+++ js/src/core/Object3D.js
@@ -3,6 +3,15 @@ var Object3DAutogen = require('./Object3D.autogen');
 
 var Object3DModel = Object3DAutogen.Object3DModel.extend({
 
+    onChildChanged: function(model, options) {
+        if (model === this.get('parent')) {
+            // Parent shouldn't be considered a child!
+            return;
+        }
+        Object3DAutogen.Object3DModel.prototype.onChildChanged.apply(this, arguments);
+
+    },
+
 });
 
 module.exports = {
