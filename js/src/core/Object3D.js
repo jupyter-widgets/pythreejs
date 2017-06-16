a3503df3cff4fbbf4414bd99edb78eca9698bade
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-06-16 11:25:33 +0200

    Cleanup

--- js/src/core/Object3D.js
+++ js/src/core/Object3D.js
@@ -1,14 +1,14 @@
 var _ = require('underscore');
-var Object3DAutogen = require('./Object3D.autogen');
+var Object3DAutogen = require('./Object3D.autogen').Object3DModel;
 
-var Object3DModel = Object3DAutogen.Object3DModel.extend({
+var Object3DModel = Object3DAutogen.extend({
 
     onChildChanged: function(model, options) {
         if (model === this.get('parent')) {
             // Parent shouldn't be considered a child!
             return;
         }
-        Object3DAutogen.Object3DModel.prototype.onChildChanged.apply(this, arguments);
+        Object3DAutogen.prototype.onChildChanged.apply(this, arguments);
 
     },
 
