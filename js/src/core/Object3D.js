9a54143af049ab6c373b3448bb47d1c1b4e819f3
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-08-22 13:33:06 +0200

    Fix child assignments

--- js/src/core/Object3D.js
+++ js/src/core/Object3D.js
@@ -3,6 +3,12 @@ var Object3DAutogen = require('./Object3D.autogen').Object3DModel;
 
 var Object3DModel = Object3DAutogen.extend({
 
+    createPropertiesArrays: function() {
+        Object3DAutogen.prototype.createPropertiesArrays.call(this);
+
+        this.property_assigners['children'] = 'assignChildren';
+    },
+
     onChildChanged: function(model, options) {
         if (model === this.get('parent')) {
             // Parent shouldn't be considered a child!
@@ -11,6 +17,18 @@ var Object3DModel = Object3DAutogen.extend({
         Object3DAutogen.prototype.onChildChanged.apply(this, arguments);
 
     },
+
+    assignChildren: function(obj, key, value) {
+        old = obj[key];
+        removed = _.difference(old, value);
+        added = _.difference(value, old);
+        if (removed.length > 0) {
+            obj.remove(...removed);
+        }
+        if (added.length > 0) {
+            obj.add(...added);
+        }
+    }
 
 });
 
