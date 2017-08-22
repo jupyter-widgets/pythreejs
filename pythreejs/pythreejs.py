9244e39dff62889787dd733b72149e973e79e034
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-08-22 13:32:50 +0200

    Fix SurfaceGeometry/Grid for example

--- pythreejs/pythreejs.py
+++ pythreejs/pythreejs.py
@@ -15,27 +15,29 @@ Another resource to understanding three.js decisions is the Udacity course on
 from __future__ import absolute_import
 
 from ipywidgets import Widget, widget_serialization, Color
-from traitlets import Unicode, CInt, Instance, Enum, List, CFloat, Bool
+from traitlets import Unicode, CInt, Instance, List, CFloat, Bool, observe, validate
 import numpy as np
 
 from ._package import npm_pkg_name
-from .enums import Shading, Colors
 from ._version import EXTENSION_VERSION
 
 
-from .core.Object3D import Object3D
-from .core.Geometry_autogen import Geometry
+from .core.BufferAttribute import BufferAttribute
 from .geometries.BoxGeometry_autogen import BoxGeometry
+from .geometries.PlainGeometry import PlainGeometry
+from .geometries.PlainBufferGeometry import PlainBufferGeometry
 from .geometries.SphereGeometry_autogen import SphereGeometry
 from .lights.AmbientLight_autogen import AmbientLight
 from .lights.DirectionalLight_autogen import DirectionalLight
 from .materials.Material_autogen import Material
 from .materials.MeshLambertMaterial_autogen import MeshLambertMaterial
 from .materials.SpriteMaterial_autogen import SpriteMaterial
+from .objects.Group_autogen import Group
+from .objects.Line_autogen import Line
 from .objects.Mesh_autogen import Mesh
 from .objects.Sprite_autogen import Sprite
 from .textures.Texture_autogen import Texture
-from .textures.DataTexture_autogen import DataTexture
+from .textures.DataTexture import DataTexture
 from .textures.TextTexture_autogen import TextTexture
 
 
@@ -50,22 +52,68 @@ def vector2(trait_type=CFloat, default=None, **kwargs):
     return List(trait_type, default_value=default, minlen=2, maxlen=2, **kwargs)
 
 
+def grid_indices_gen(nx, ny):
+    for x in range(nx - 1):
+        for y in range(ny - 1):
+            root = x + y * ny
+            yield (root, root + 1, root + nx)
+            yield (root + nx, root + 1, root + nx + 1)
 
 
-class SurfaceGeometry(Geometry):
+class SurfaceGeometry(PlainBufferGeometry):
     """
     A regular grid with heights
     """
-    _view_name = Unicode('SurfaceGeometryView').tag(sync=True)
-    _model_name = Unicode('SurfaceGeometryModel').tag(sync=True)
+    z = List(CFloat, [0] * 100)
+    width = CInt(10)
+    height = CInt(10)
+    width_segments = CInt(10)
+    height_segments = CInt(10)
 
-    z = List(CFloat, [0] * 100).tag(sync=True)
-    width = CInt(10).tag(sync=True)
-    height = CInt(10).tag(sync=True)
-    width_segments = CInt(10).tag(sync=True)
-    height_segments = CInt(10).tag(sync=True)
+    @observe('z', 'width', 'height', 'width_segments', 'height_segments')
+    def _update_surface(self, change):
+        nx = self.width_segments + 1
+        ny = self.height_segments + 1
+        x = np.linspace(-self.width/2, self.width/2, nx)
+        y = np.linspace(-self.height/2, self.height/2, ny)
+        xx, yy = np.meshgrid(x, y)
+        z = np.array(self.z).reshape((nx, ny))
+
+        positions = np.dstack((xx, yy, z)).reshape(nx * ny, 3).astype(np.float32)
+
+        dx, dy = np.gradient(z, self.width/nx, self.height/ny)
+        normals = np.dstack((-dx, -dy, np.ones_like(dx))).reshape(nx * ny, 3).astype(np.float32)
+
+        vmin = np.min(positions, 0)
+        vrange = np.max(positions, 0) - vmin
+        uvs = ((positions - vmin) / vrange)[:, :2]
+
+        self.attributes = {
+            'position': BufferAttribute(positions),
+            'index': BufferAttribute(np.array(tuple(grid_indices_gen(nx, ny)), dtype=np.uint16).ravel()),
+            'normal': BufferAttribute(normals),
+            'uv': BufferAttribute(uvs),
+        }
 
 
+def SurfaceGrid(geometry, material):
+    """A grid covering a surface.
+
+    This will draw a line mesh overlaying the SurfaceGeometry.
+    """
+    nx = geometry.width_segments + 1
+    ny = geometry.height_segments + 1
+    vertices = geometry.attributes['position'].array
+
+    lines = []
+    for x in range(nx):
+        g = PlainGeometry(vertices=[vertices[y * nx + x, :].tolist() for y in range(ny)])
+        lines.append(Line(g, material))
+    for y in range(ny):
+        g = PlainGeometry(vertices=[vertices[y * nx + x, :].tolist() for x in range(nx)])
+        lines.append(Line(g, material))
+
+    return Group(children=lines)
 
 
 class ParticleSystemMaterial(Material):
@@ -175,18 +223,6 @@ def lights_gray():
         DirectionalLight(position=[1, 1, 1], color=[.5, .5, .5]),
         DirectionalLight(position=[-1, -1, -1], color=[.7, .7, .7]),
     ]
-
-
-class SurfaceGrid(Mesh):
-    """A grid covering a surface.
-
-    This will draw a line mesh overlaying the SurfaceGeometry.
-    """
-    _view_name = Unicode('SurfaceGridView').tag(sync=True)
-    _model_name = Unicode('SurfaceGridModel').tag(sync=True)
-
-    geometry = Instance(SurfaceGeometry).tag(sync=True, **widget_serialization)
-    material = Instance(Material).tag(sync=True, **widget_serialization)
 
 
 def make_text(text, position=(0, 0, 0), height=1):
