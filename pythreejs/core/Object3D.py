b13b33ded14a0ca7e8efdf1295d2547fa647bcca
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-09-15 15:14:29 +0200

    Fix Object3D add/remove on python side

--- pythreejs/core/Object3D.py
+++ pythreejs/core/Object3D.py
@@ -7,14 +7,22 @@ from .Object3D_autogen import Object3D as Object3DBase
 @register
 class Object3D(Object3DBase):
 
-    def add(self, child):
+    def add(self, children):
+        if isinstance(children, Object3D):
+            children = [children]
         children_list = list(self.children)
-        children_list.append(child)
+        children_list.extend(children)
 
         self.children = tuple(children_list)
 
-    def remove(self, child):
-        pass
+    def remove(self, children):
+        if isinstance(children, Object3D):
+            children = [children]
+        children_list = list(self.children)
+        for child in children:
+            children_list.remove(child)
+
+        self.children = tuple(children_list)
 
     def lookAt(self, vector):
         self.exec_three_obj_method('lookAt', vector)
