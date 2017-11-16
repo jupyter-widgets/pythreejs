d54e1a3c06924a5eb27b30612e2d25bc77b7e141
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-11-16 10:42:28 +0100

    Change enums to namespace for tab-completion

--- pythreejs/enums.py
+++ pythreejs/enums.py
@@ -1,203 +1,219 @@
 """
 This file auto-generated with generate-enums.js
-Date: Fri Jun 02 2017 16:44:47 GMT+0200 (W. Europe Daylight Time)
+Date: Wed Nov 15 2017 17:14:48 GMT+0100 (W. Europe Standard Time)
 """
 
+class EnumNamespace:
+    """A collection of enum values.
 
-Equations = [
-    'AddEquation',
-    'SubtractEquation',
-    'ReverseSubtractEquation',
-    'MinEquation',
-    'MaxEquation',
-]
+    This collection can be used as specification to traitlets.Enum
+    as it supports the `in` operator, and also supports tab completion
+    as the values are attributes.
+    """
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
 
-BlendFactors = [
-    'ZeroFactor',
-    'OneFactor',
-    'SrcColorFactor',
-    'OneMinusSrcColorFactor',
-    'SrcAlphaFactor',
-    'OneMinusSrcAlphaFactor',
-    'DstAlphaFactor',
-    'OneMinusDstAlphaFactor',
-    'DstColorFactor',
-    'OneMinusDstColorFactor',
-    'SrcAlphaSaturateFactor',
-]
+    def __contains__(self, key):
+        return key in self.__dict__
 
-Side = [
-    'FrontSide',
-    'BackSide',
-    'DoubleSide',
-]
+    def __repr__(self):
+        return str(list(filter(lambda e: not e.startswith('_'), dir(self))))
 
-Shading = [
-    'FlatShading',
-    'SmoothShading',
-]
 
-Colors = [
-    'NoColors',
-    'FaceColors',
-    'VertexColors',
-]
+Equations = EnumNamespace(
+    AddEquation='AddEquation',
+    SubtractEquation='SubtractEquation',
+    ReverseSubtractEquation='ReverseSubtractEquation',
+    MinEquation='MinEquation',
+    MaxEquation='MaxEquation',
+)
 
-BlendingMode = [
-    'NoBlending',
-    'NormalBlending',
-    'AdditiveBlending',
-    'SubtractiveBlending',
-    'MultiplyBlending',
-    'CustomBlending',
-]
+BlendFactors = EnumNamespace(
+    ZeroFactor='ZeroFactor',
+    OneFactor='OneFactor',
+    SrcColorFactor='SrcColorFactor',
+    OneMinusSrcColorFactor='OneMinusSrcColorFactor',
+    SrcAlphaFactor='SrcAlphaFactor',
+    OneMinusSrcAlphaFactor='OneMinusSrcAlphaFactor',
+    DstAlphaFactor='DstAlphaFactor',
+    OneMinusDstAlphaFactor='OneMinusDstAlphaFactor',
+    DstColorFactor='DstColorFactor',
+    OneMinusDstColorFactor='OneMinusDstColorFactor',
+    SrcAlphaSaturateFactor='SrcAlphaSaturateFactor',
+)
 
-DepthMode = [
-    'NeverDepth',
-    'AlwaysDepth',
-    'LessDepth',
-    'LessEqualDepth',
-    'EqualDepth',
-    'GreaterEqualDepth',
-    'GreaterDepth',
-    'NotEqualDepth',
-]
+Side = EnumNamespace(
+    FrontSide='FrontSide',
+    BackSide='BackSide',
+    DoubleSide='DoubleSide',
+)
 
-Operations = [
-    'MultiplyOperation',
-    'MixOperation',
-    'AddOperation',
-]
+Shading = EnumNamespace(
+    FlatShading='FlatShading',
+    SmoothShading='SmoothShading',
+)
 
-MappingModes = [
-    'UVMapping',
-    'CubeReflectionMapping',
-    'CubeRefractionMapping',
-    'EquirectangularReflectionMapping',
-    'EquirectangularRefractionMapping',
-    'SphericalReflectionMapping',
-    'CubeUVReflectionMapping',
-    'CubeUVRefractionMapping',
-]
+Colors = EnumNamespace(
+    NoColors='NoColors',
+    FaceColors='FaceColors',
+    VertexColors='VertexColors',
+)
 
-WrappingModes = [
-    'RepeatWrapping',
-    'ClampToEdgeWrapping',
-    'MirroredRepeatWrapping',
-]
+BlendingMode = EnumNamespace(
+    NoBlending='NoBlending',
+    NormalBlending='NormalBlending',
+    AdditiveBlending='AdditiveBlending',
+    SubtractiveBlending='SubtractiveBlending',
+    MultiplyBlending='MultiplyBlending',
+    CustomBlending='CustomBlending',
+)
 
-Filters = [
-    'NearestFilter',
-    'NearestMipMapNearestFilter',
-    'NearestMipMapLinearFilter',
-    'LinearFilter',
-    'LinearMipMapNearestFilter',
-    'LinearMipMapLinearFilter',
-]
+DepthMode = EnumNamespace(
+    NeverDepth='NeverDepth',
+    AlwaysDepth='AlwaysDepth',
+    LessDepth='LessDepth',
+    LessEqualDepth='LessEqualDepth',
+    EqualDepth='EqualDepth',
+    GreaterEqualDepth='GreaterEqualDepth',
+    GreaterDepth='GreaterDepth',
+    NotEqualDepth='NotEqualDepth',
+)
 
-DataTypes = [
-    'UnsignedByteType',
-    'ByteType',
-    'ShortType',
-    'UnsignedShortType',
-    'IntType',
-    'UnsignedIntType',
-    'FloatType',
-    'HalfFloatType',
-]
+Operations = EnumNamespace(
+    MultiplyOperation='MultiplyOperation',
+    MixOperation='MixOperation',
+    AddOperation='AddOperation',
+)
 
-PixelTypes = [
-    'UnsignedShort4444Type',
-    'UnsignedShort5551Type',
-    'UnsignedShort565Type',
-    'UnsignedInt248Type',
-]
+MappingModes = EnumNamespace(
+    UVMapping='UVMapping',
+    CubeReflectionMapping='CubeReflectionMapping',
+    CubeRefractionMapping='CubeRefractionMapping',
+    EquirectangularReflectionMapping='EquirectangularReflectionMapping',
+    EquirectangularRefractionMapping='EquirectangularRefractionMapping',
+    SphericalReflectionMapping='SphericalReflectionMapping',
+    CubeUVReflectionMapping='CubeUVReflectionMapping',
+    CubeUVRefractionMapping='CubeUVRefractionMapping',
+)
 
-PixelFormats = [
-    'AlphaFormat',
-    'RGBFormat',
-    'RGBAFormat',
-    'RGBEFormat',
-    'LuminanceFormat',
-    'LuminanceAlphaFormat',
-    'DepthFormat',
-    'DepthStencilFormat',
-]
+WrappingModes = EnumNamespace(
+    RepeatWrapping='RepeatWrapping',
+    ClampToEdgeWrapping='ClampToEdgeWrapping',
+    MirroredRepeatWrapping='MirroredRepeatWrapping',
+)
 
-DepthFormats = [
-    'DepthFormat',
-    'DepthStencilFormat',
-]
+Filters = EnumNamespace(
+    NearestFilter='NearestFilter',
+    NearestMipMapNearestFilter='NearestMipMapNearestFilter',
+    NearestMipMapLinearFilter='NearestMipMapLinearFilter',
+    LinearFilter='LinearFilter',
+    LinearMipMapNearestFilter='LinearMipMapNearestFilter',
+    LinearMipMapLinearFilter='LinearMipMapLinearFilter',
+)
 
-CompressedTextureFormats = [
-    'RGB_S3TC_DXT1_Format',
-    'RGBA_S3TC_DXT1_Format',
-    'RGBA_S3TC_DXT3_Format',
-    'RGBA_S3TC_DXT5_Format',
-    'RGB_PVRTC_4BPPV1_Format',
-    'RGB_PVRTC_2BPPV1_Format',
-    'RGBA_PVRTC_4BPPV1_Format',
-    'RGBA_PVRTC_2BPPV1_Format',
-    'RGB_ETC1_Format',
-]
+DataTypes = EnumNamespace(
+    UnsignedByteType='UnsignedByteType',
+    ByteType='ByteType',
+    ShortType='ShortType',
+    UnsignedShortType='UnsignedShortType',
+    IntType='IntType',
+    UnsignedIntType='UnsignedIntType',
+    FloatType='FloatType',
+    HalfFloatType='HalfFloatType',
+)
 
-TextureEncodings = [
-    'LinearEncoding',
-    'sRGBEncoding',
-    'RGBEEncoding',
-    'LogLuvEncoding',
-    'RGBM7Encoding',
-    'RGBM16Encoding',
-    'RGBDEncoding',
-    'GammaEncoding',
-]
+PixelTypes = EnumNamespace(
+    UnsignedShort4444Type='UnsignedShort4444Type',
+    UnsignedShort5551Type='UnsignedShort5551Type',
+    UnsignedShort565Type='UnsignedShort565Type',
+    UnsignedInt248Type='UnsignedInt248Type',
+)
 
-CullFaceModes = [
-    'CullFaceNone',
-    'CullFaceBack',
-    'CullFaceFront',
-    'CullFaceFrontBack',
-]
+PixelFormats = EnumNamespace(
+    AlphaFormat='AlphaFormat',
+    RGBFormat='RGBFormat',
+    RGBAFormat='RGBAFormat',
+    RGBEFormat='RGBEFormat',
+    LuminanceFormat='LuminanceFormat',
+    LuminanceAlphaFormat='LuminanceAlphaFormat',
+    DepthFormat='DepthFormat',
+    DepthStencilFormat='DepthStencilFormat',
+)
 
-FrontFaceDirection = [
-    'FrontFaceDirectionCW',
-    'FrontFaceDirectionCCW',
-]
+DepthFormats = EnumNamespace(
+    DepthFormat='DepthFormat',
+    DepthStencilFormat='DepthStencilFormat',
+)
 
-ShadowTypes = [
-    'BasicShadowMap',
-    'PCFShadowMap',
-    'PCFSoftShadowMap',
-]
+CompressedTextureFormats = EnumNamespace(
+    RGB_S3TC_DXT1_Format='RGB_S3TC_DXT1_Format',
+    RGBA_S3TC_DXT1_Format='RGBA_S3TC_DXT1_Format',
+    RGBA_S3TC_DXT3_Format='RGBA_S3TC_DXT3_Format',
+    RGBA_S3TC_DXT5_Format='RGBA_S3TC_DXT5_Format',
+    RGB_PVRTC_4BPPV1_Format='RGB_PVRTC_4BPPV1_Format',
+    RGB_PVRTC_2BPPV1_Format='RGB_PVRTC_2BPPV1_Format',
+    RGBA_PVRTC_4BPPV1_Format='RGBA_PVRTC_4BPPV1_Format',
+    RGBA_PVRTC_2BPPV1_Format='RGBA_PVRTC_2BPPV1_Format',
+    RGB_ETC1_Format='RGB_ETC1_Format',
+)
 
-ToneMappings = [
-    'NoToneMapping',
-    'LinearToneMapping',
-    'ReinhardToneMapping',
-    'Uncharted2ToneMapping',
-    'CineonToneMapping',
-]
+TextureEncodings = EnumNamespace(
+    LinearEncoding='LinearEncoding',
+    sRGBEncoding='sRGBEncoding',
+    RGBEEncoding='RGBEEncoding',
+    LogLuvEncoding='LogLuvEncoding',
+    RGBM7Encoding='RGBM7Encoding',
+    RGBM16Encoding='RGBM16Encoding',
+    RGBDEncoding='RGBDEncoding',
+    GammaEncoding='GammaEncoding',
+)
 
-LoopModes = [
-    'LoopOnce',
-    'LoopRepeat',
-    'LoopPingPong',
-]
+CullFaceModes = EnumNamespace(
+    CullFaceNone='CullFaceNone',
+    CullFaceBack='CullFaceBack',
+    CullFaceFront='CullFaceFront',
+    CullFaceFrontBack='CullFaceFrontBack',
+)
 
-InterpolationModes = [
-    'InterpolateDiscrete',
-    'InterpolateLinear',
-    'InterpolateSmooth',
-]
+FrontFaceDirection = EnumNamespace(
+    FrontFaceDirectionCW='FrontFaceDirectionCW',
+    FrontFaceDirectionCCW='FrontFaceDirectionCCW',
+)
 
-EndingModes = [
-    'ZeroCurvatureEnding',
-    'ZeroSlopeEnding',
-    'WrapAroundEnding',
-]
+ShadowTypes = EnumNamespace(
+    BasicShadowMap='BasicShadowMap',
+    PCFShadowMap='PCFShadowMap',
+    PCFSoftShadowMap='PCFSoftShadowMap',
+)
 
-DrawModes = [
-    'TrianglesDrawMode',
-    'TriangleStripDrawMode',
-    'TriangleFanDrawMode',
-]
+ToneMappings = EnumNamespace(
+    NoToneMapping='NoToneMapping',
+    LinearToneMapping='LinearToneMapping',
+    ReinhardToneMapping='ReinhardToneMapping',
+    Uncharted2ToneMapping='Uncharted2ToneMapping',
+    CineonToneMapping='CineonToneMapping',
+)
+
+LoopModes = EnumNamespace(
+    LoopOnce='LoopOnce',
+    LoopRepeat='LoopRepeat',
+    LoopPingPong='LoopPingPong',
+)
+
+InterpolationModes = EnumNamespace(
+    InterpolateDiscrete='InterpolateDiscrete',
+    InterpolateLinear='InterpolateLinear',
+    InterpolateSmooth='InterpolateSmooth',
+)
+
+EndingModes = EnumNamespace(
+    ZeroCurvatureEnding='ZeroCurvatureEnding',
+    ZeroSlopeEnding='ZeroSlopeEnding',
+    WrapAroundEnding='WrapAroundEnding',
+)
+
+DrawModes = EnumNamespace(
+    TrianglesDrawMode='TrianglesDrawMode',
+    TriangleStripDrawMode='TriangleStripDrawMode',
+    TriangleFanDrawMode='TriangleFanDrawMode',
+)
