c674b5d6dc40b110b314059bfd9451977c6042b8
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-02-06 10:50:10 +0100

    Revert "Ensure example image resources are available"
    This reverts commit c32ccbce49144bdf3152e4002cefde440e99351d.

--- js/scripts/copy-files.js
+++ js/scripts/copy-files.js
@@ -12,9 +12,6 @@ const staticDir = path.resolve(baseDir, '..', 'pythreejs', 'static');
 const buildDir = path.resolve(baseDir, 'dist');
 const docStaticDir = path.resolve(baseDir, '..', 'docs', 'source', '_static');
 
-const exampleImagesSrcDir = path.resolve(baseDir, '..', 'examples', 'img');
-const exampleImagesDstDir = path.resolve(baseDir, '..', 'docs', 'source', 'examples', 'img');
-
 
 function copyThree() {
     return fse.copy(
@@ -34,25 +31,10 @@ function copyBundleToDocs() {
     });
 }
 
-function copyExampleImagesToDocs() {
-    return fse.readdir(exampleImagesSrcDir).then((dirFiles) => {
-        return Promise.all(dirFiles.map(filePath => {
-            return fse.copy(
-                path.resolve(exampleImagesSrcDir, filePath),
-                path.resolve(exampleImagesDstDir, filePath)
-            ).then(function() {
-                console.log(`Copied ${filePath} to docs examples' image folder`);
-            });
-        }));
-    });
-}
-
-
 if (require.main === module) {
     // This script copies files after a build
     Promise.all([
         copyThree(),
         copyBundleToDocs(),
-        copyExampleImagesToDocs(),
     ]);
 }
