95657831a2d26d3bc6cb164896c2a233fd0435c2
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-03-08 15:50:24 +0100

    Ensure __init__.py on all levels

--- js/scripts/generate-wrappers.js
+++ js/scripts/generate-wrappers.js
@@ -921,9 +921,18 @@ function createPythonWrapper(modulePath, className) {
 
 function createPythonModuleInitFile(modulePath) {
 
-    const dirname = path.dirname(modulePath);
-    const pyInitFilePath = path.resolve(pySrcDir, dirname, '__init__.py');
-    return fse.ensureFile(pyInitFilePath);
+    let dirname = path.dirname(path.resolve(pySrcDir, modulePath));
+    let pyInitFilePath;
+    const promises = [];
+    while (dirname !== pySrcDir) {
+        pyInitFilePath = path.join(dirname, '__init__.py');
+        promises.push(fse.ensureFile(pyInitFilePath));
+        if (dirname === path.dirname(dirname)) {
+            throw new Error(`${dirname}, ${path.dirname(dirname)}`);
+        }
+        dirname = path.dirname(dirname);
+    }
+    return Promise.all(promises);
 }
 
 
