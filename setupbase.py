7fba16b16ce7da0fe76de47beb47fc338b354f6d
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-03-08 17:57:39 +0100

    Update setupbase

--- setupbase.py
+++ setupbase.py
@@ -212,7 +212,12 @@ def run(cmd, **kwargs):
     kwargs.setdefault('shell', os.name == 'nt')
     if not isinstance(cmd, (list, tuple)) and os.name != 'nt':
         cmd = shlex.split(cmd)
-    cmd[0] = which(cmd[0])
+    cmd_path = which(cmd[0])
+    if not cmd_path:
+        sys.exit("Aborting. Could not find cmd (%s) in path. "
+                 "If command is not expected to be in user's path, "
+                 "use an absolute path." % cmd[0])
+    cmd[0] = cmd_path
     return subprocess.check_call(cmd, **kwargs)
 
 
