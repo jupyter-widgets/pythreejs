dae8e712b3c668aea1c078d1e2af6fbd23cc0387
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-03-08 14:37:46 +0100

    Ensure we corrently listen for array/instance attrs

--- js/src/_base/Three.js
+++ js/src/_base/Three.js
@@ -24,30 +24,58 @@ function listenNested(model, propNames, callback) {
     propNames.forEach(function(propName) {
         // listen to current values in array
         var curr = model.get(propName) || [];
-        // ignore ThreeModels in order to support properties
-        // that are either an instance, or a sequence of instances:
+        // support properties that are either an instance, or a
+        // sequence of instances:
         if (curr instanceof ThreeModel) {
-            return;
+            model.listenTo(curr, 'change', callback);
+            model.listenTo(curr, 'childchange', callback);
+        } else {
+            utils.childModelsNested(curr).forEach(function(childModel) {
+                model.listenTo(childModel, 'change', callback);
+                model.listenTo(childModel, 'childchange', callback);
+            });
         }
-        utils.childModelsNested(curr).forEach(function(childModel) {
-            model.listenTo(childModel, 'change', callback);
-            model.listenTo(childModel, 'childchange', callback);
-        });
 
         // make sure to (un)hook listeners when array changes
         model.on('change:' + propName, function(model, value) {
             var prev = model.previous(propName) || [];
             var curr = value || [];
 
-            var diff = utils.nestedDiff(curr, prev);
+            // Check for instance values:
+            if (prev instanceof ThreeModel) {
+                model.stopListening(prev);
+            }
+            if (curr instanceof ThreeModel) {
+                model.listenTo(curr, 'change', callback);
+                model.listenTo(curr, 'childchange', callback);
+            }
+            // Done if both are instance values:
+            if (prev instanceof ThreeModel && curr instanceof ThreeModel) {
+                return;
+            }
+            if (prev instanceof ThreeModel) {
+                // Implies curr is array
+                utils.childModelsNested(curr).forEach(function(childModel) {
+                    model.listenTo(childModel, 'change', callback);
+                    model.listenTo(childModel, 'childchange', callback);
+                });
+            } else if (curr instanceof ThreeModel) {
+                // Implies prev is array
+                utils.childModelsNested(prev).forEach(function(childModel) {
+                    model.stopListening(childModel);
+                });
+            } else {
+                // Both are arrays
+                var diff = utils.nestedDiff(curr, prev);
 
-            diff.added.forEach(function(childModel) {
-                model.listenTo(childModel, 'change', callback);
-                model.listenTo(childModel, 'childchange', callback);
-            });
-            diff.removed.forEach(function(childModel) {
-                model.stopListening(childModel);
-            });
+                diff.added.forEach(function(childModel) {
+                    model.listenTo(childModel, 'change', callback);
+                    model.listenTo(childModel, 'childchange', callback);
+                });
+                diff.removed.forEach(function(childModel) {
+                    model.stopListening(childModel);
+                });
+            }
         });
     });
 }
