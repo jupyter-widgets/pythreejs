24c3c72e32cb04f441aef328beefc6f49f907e4a
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-03-19 13:39:39 +0100

    Cleanup setup.py classifiers

--- setup.py
+++ setup.py
@@ -98,16 +98,14 @@ setup_args = {
     'url': 'https://github.com/jovyan/pythreejs',
     'keywords': ['ipython', 'jupyter', 'widgets', 'webgl', 'graphics', '3d'],
     'classifiers': [
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Multimedia :: Graphics',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: JavaScript,'
     ],
 }
 
