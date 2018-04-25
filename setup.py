b6a81f195b49158aaec6e13087f4a60e920cfd76
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-04-25 15:08:43 +0200

    Need fix in latest ipywidgets

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup_args = {
     'license': 'BSD',
     'include_package_data': True,
     'install_requires': [
-        'ipywidgets>=7.2',
+        'ipywidgets>=7.2.1',
         'ipydatawidgets>=1.1.1',
         'numpy',
     ],
